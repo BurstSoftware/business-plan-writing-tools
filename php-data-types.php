ea0262dd402e64184f87c842b253f75d467fb011
Nathan Rossow <nathanrossow@gmail.com>
2026-08-08 18:28:30 -0500

    Delete php-data-types.php

--- php-data-types.php
+++
@@ -1,6 +0,0 @@
-$name = "Nathan";               // String
-$age = 35;                      // Integer
-$price = 19.99;                 // Float
-$isPublished = true;            // Boolean
-$nothing = null;                // Null
-$tags = ["PHP", "HTMX", "Web"]; // Array
